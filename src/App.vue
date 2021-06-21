fbe8f265cddbe7752d87281b62957dc42b3f649b
Baby777 <409178491@qq.com>
2021-06-21 13:52:55 +0800

    pro init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    helo word
   </div>
 </template>
 
@@ -11,13 +10,9 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" scoped rel="stylesheet/stylus">
+@import "~common/stylus/variable.styl";
+
+  #app
+    color : $color-theme
 </style>
